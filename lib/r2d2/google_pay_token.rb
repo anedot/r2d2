3f8dff8b9970661baa4d54382e4a4452fa5efbf3
Spencer Oberstadt <spencer.oberstadt@anedot.com>
2022-05-09 15:28:54 -0400

    Don't prefix recipient_id

--- lib/r2d2/google_pay_token.rb
+++ lib/r2d2/google_pay_token.rb
@@ -94,7 +94,7 @@ module R2D2
     end
 
     def payload_signature_verified?
-      signed_string_message = [SENDER_ID, ecv2_recipient_id, protocol_version, signed_message].map do |str|
+      signed_string_message = [SENDER_ID, recipient_id, protocol_version, signed_message].map do |str|
         [str.length].pack('V') + str
       end.join
 
@@ -131,10 +131,6 @@ module R2D2
 
     def intermediate_signing_key_signed_key
       @intermediate_signing_key_signed_key ||= JSON.parse(intermediate_signing_key['signedKey'])
-    end
-
-    def ecv2_recipient_id
-      "merchant:#{recipient_id}"
     end
 
     def current_time_ms
